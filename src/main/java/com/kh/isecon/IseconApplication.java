18ee4b8e7db4d9cf2715bdb697a65970d0e18b85
leehal <annaleehalim@gmail.com>
2024-05-08 18:48:27 +0900

    Initial commit

---
+++ src/main/java/com/kh/isecon/IseconApplication.java
@@ -0,0 +1,13 @@
+package com.kh.isecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IseconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IseconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/isecon/IseconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.isecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IseconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
