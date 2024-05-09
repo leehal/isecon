3b137f687dd99ceb633a6a8a1cf783d99496a0dc
hojunahn <hojunahn@naver.com>
2024-05-09 18:56:12 +0900

    user and sale vo clear

--- src/main/java/com/kh/isecon/VO/SaleVo.java
+++ src/main/java/com/kh/isecon/VO/SaleVo.java
@@ -1,4 +1,17 @@
 package com.kh.isecon.VO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaleVo {
+    private int sno;
+    private int uno;
+    private int pno;
+
 }
--- src/main/java/com/kh/isecon/VO/UsersVo.java
+++ src/main/java/com/kh/isecon/VO/UsersVo.java
@@ -1,4 +1,19 @@
 package com.kh.isecon.VO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsersVo {
+    private int uno;
+    private String id;
+    private String phone;
+    private String address;
+    private char admin;
+    private String nickname;
 }
