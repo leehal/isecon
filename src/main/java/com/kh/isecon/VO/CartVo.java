42ae7ce9112af4970ae121e4fc6fa98125560f94
SYW <dyddnr33@naver.com>
2024-05-09 19:19:05 +0900

    cart and product vo clear

--- src/main/java/com/kh/isecon/VO/CartVo.java
+++ src/main/java/com/kh/isecon/VO/CartVo.java
@@ -1,4 +1,19 @@
+
 package com.kh.isecon.VO;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CartVo {
+    private int cno;
+    private int uno;
+    private int pno;
 }
--- src/main/java/com/kh/isecon/VO/ProductVo.java
+++ src/main/java/com/kh/isecon/VO/ProductVo.java
@@ -1,4 +1,20 @@
 package com.kh.isecon.VO;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class ProductVo {
+    private int pno;
+    private String pname;
+    private int price;
+    private String option;
+    private String pimg;
 }
