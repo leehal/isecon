22baf0d762dd17f00782d500dc7812c26c61995a
leehal <annaleehalim@gmail.com>
2024-05-09 18:52:24 +0900

    MUSIC , PLAYLIST VO 추가

--- src/main/java/com/kh/isecon/DAO/MusicDao.java
+++ src/main/java/com/kh/isecon/DAO/MusicDao.java
@@ -1,4 +1,13 @@
 package com.kh.isecon.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MusicDao {
+    Connection conn = null;
+    Statement stmt = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
 }
--- src/main/java/com/kh/isecon/VO/MusicVo.java
+++ src/main/java/com/kh/isecon/VO/MusicVo.java
@@ -1,4 +1,17 @@
 package com.kh.isecon.VO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MusicVo {
+    private int mno;
+    private String mname;
+    private String singer;
+    private String surl;
 }
