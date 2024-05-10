52549433e1f2d704b58d2a941183c8eb05f97c6f
SYW <dyddnr33@naver.com>
2024-05-10 15:19:59 +0900

    PRODUCT, CART DAO

--- src/main/java/com/kh/isecon/DAO/CartDao.java
+++ src/main/java/com/kh/isecon/DAO/CartDao.java
@@ -1,4 +1,78 @@
 package com.kh.isecon.DAO;
 
+import com.kh.isecon.COMMON.Common;
+import com.kh.isecon.VO.CartVo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartDao {
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    public void cartInsert(int pno, int uno) {
+
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String query = "INSERT INTO CART (CNO, UNO, PNO) VALUES (CNO_SEQ.NEXTVAL," + uno + pno + ")";
+            rs = stmt.executeQuery(query);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
+    }
+
+    public void cartDelete(int cno) {
+
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String query = "DELETE FROM CART WHERE CNO=" + cno;
+            rs = stmt.executeQuery(query);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
+    }
+
+    public List<CartVo> CartSelect() {
+        List<CartVo> list = new ArrayList<>();
+
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String query = "SELECT * FROM CART";
+            rs = stmt.executeQuery(query);
+
+            CartVo vo = new CartVo();
+
+            while (rs.next()) {
+                int cno = rs.getInt("CNO");
+                int uno = rs.getInt("UNO");
+                int pno = rs.getInt("PNO");
+
+                vo.setCno(cno);
+                vo.setUno(uno);
+                vo.setPno(pno);
+                list.add(vo);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
+
--- src/main/java/com/kh/isecon/DAO/ProductDao.java
+++ src/main/java/com/kh/isecon/DAO/ProductDao.java
@@ -7,12 +7,41 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDao {
     Connection conn = null;
     Statement stmt = null;
-    PreparedStatement pstmt = null;
     ResultSet rs = null;
+
+    public List<ProductVo> productSelectList() { // 굿즈 페이지 상품 전체 보여주기
+        List<ProductVo> list = new ArrayList<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String query = "SELECT * FROM PRODUCT";
+            rs = stmt.executeQuery(query);
+
+            while(rs.next()) {
+                int pno = rs.getInt("PNO");
+                String pname = rs.getString("PNAME");
+                int price = rs.getInt("PRICE");
+                String option = rs.getString("OPTION");
+                String pimg = rs.getString("PIMG");
+
+                ProductVo vo = new ProductVo(pno, pname, price, option, pimg);
+                list.add(vo);
+            }
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 
     public ProductVo productDetail (int pno){ // 해당 굿즈의 상세 페이지를 보여주기
     ProductVo vo = new ProductVo();
@@ -22,14 +51,15 @@ public class ProductDao {
             String query = "SELELTE * FROM PRODUCT WHERE PNO =" + pno;
             rs = stmt.executeQuery(query);
 
-            pno = rs.getInt("PNO");
-            String pname = rs.getString("PNAME");
-            int price = rs.getInt("PRICE");
-            String option = rs.getString("OPTIONS");
-            String pimg = rs.getString("PIMG");
+            while(rs.next()) {
+                pno = rs.getInt("PNO");
+                String pname = rs.getString("PNAME");
+                int price = rs.getInt("PRICE");
+                String option = rs.getString("OPTIONS");
+                String pimg = rs.getString("PIMG");
 
-            vo = new ProductVo(pno, pname, price, option, pimg);
-
+                vo = new ProductVo(pno, pname, price, option, pimg);
+            }
             Common.close(rs);
             Common.close(stmt);
             Common.close(conn);
@@ -40,4 +70,5 @@ public class ProductDao {
         }
         return vo;
     }
+
 }
