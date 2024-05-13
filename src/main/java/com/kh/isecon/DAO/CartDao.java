b54ea85c85a9a9984984c2046b40f39c88764b07
SYW <dyddnr33@naver.com>
2024-05-13 15:45:25 +0900

    CartDao Update

--- src/main/java/com/kh/isecon/DAO/CartDao.java
+++ src/main/java/com/kh/isecon/DAO/CartDao.java
@@ -2,6 +2,7 @@ package com.kh.isecon.DAO;
 
 import com.kh.isecon.COMMON.Common;
 import com.kh.isecon.VO.CartVo;
+import com.kh.isecon.VO.ProductVo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -93,6 +94,26 @@ public class CartDao {
         }
     }
 
+    public int CartReturnPno(int cno) {
+        int pno = 0;
 
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String query = "SELECT PNO FROM CART WHERE CNO = " + cno;
+            rs = stmt.executeQuery(query);
+
+            pno = rs.getInt("PNO");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
+
+        return pno;
+}
 }
 
